f1612f93b03ea59e91cea9d2eb8691b7ea10196c
haesa <xdhaesa@gmail.com>
2024-11-18 06:54:36 +0900

    #1 [UI] 미로그인 시 헤더 컴포넌트

--- src/components/Header/BoardMenu.jsx
+++ src/components/Header/BoardMenu.jsx
@@ -4,11 +4,11 @@ import styles from './BoardMenu.module.css';
 
 export default function BoardMenu({ to, name }) {
   const { pathname } = useLocation();
-  const isSeleced = to === pathname && styles.select;
+  const isSeleced = pathname === to && styles.select;
 
   return (
     <Link to={to} className={isSeleced}>
-      <li className={`${styles.menu} `}>
+      <li className={styles.menu}>
         <span className={styles.name}>{name}</span>
       </li>
     </Link>
