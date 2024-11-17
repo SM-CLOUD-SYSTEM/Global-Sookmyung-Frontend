77aba99d32b2dbcf964ed4151c76977ce47a7ed4
haesa <xdhaesa@gmail.com>
2024-11-18 02:41:05 +0900

    #1 [CHORE] constants와 assets 폴더에 대한 절대 경로 설정 추가

--- craco.config.js
+++ craco.config.js
@@ -9,6 +9,8 @@ module.exports = {
       '@components': path.resolve(__dirname, 'src/components/'),
       '@pages': path.resolve(__dirname, 'src/pages/'),
       '@utils': path.resolve(__dirname, 'src/utils/'),
+      '@constants': path.resolve(__dirname, 'src/constants/'),
+      '@assets': path.resolve(__dirname, 'src/assets/'),
     },
   },
 };
