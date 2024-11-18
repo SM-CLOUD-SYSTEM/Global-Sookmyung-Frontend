925754bcb84b5afdfd54501b3546f5db37c8197d
haesa <xdhaesa@gmail.com>
2024-11-18 18:34:54 +0900

    #1 [UI] ActionInput 컴포넌트

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom';
 
 import { Header } from '@components';
+
 import styles from './App.module.css';
 
 function App() {
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+export { default as ActionInput } from '@components/ActionInput';
 export { default as Button } from '@components/Button';
 export { default as Header } from '@components/Header';
 export { default as Input } from '@components/Input';
