5840b4c497c6c1da62af03d21ef9c30c246aae6f
haesa <xdhaesa@gmail.com>
2024-11-18 19:50:46 +0900

    #1 [UI] CheckCircle, CheckBox 컴포넌트

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 export { default as ActionInput } from '@components/ActionInput';
 export { default as Button } from '@components/Button';
+export { default as CheckBox } from '@components/CheckBox';
+export { default as CheckCircle } from '@components/CheckCircle';
 export { default as Header } from '@components/Header';
 export { default as Input } from '@components/Input';
