6d9e5bc67b1834488a2b63e13eb187f037a85f09
haesa <xdhaesa@gmail.com>
2024-11-20 05:07:21 +0900

    #5 [MODIFY] Button 컴포넌트에 onClick prop 추가
    버튼 트랜지션 추가

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,13 +1,19 @@
 import { BUTTON_SIZE } from '@constants';
+
 import styles from './Button.module.css';
 
 export default function Button({
   children,
   size = BUTTON_SIZE.regular,
+  onClick,
   disabled = false,
 }) {
   return (
-    <button className={`${styles.button} ${styles[size]}`} disabled={disabled}>
+    <button
+      className={`${styles.button} ${styles[size]}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
       {children}
     </button>
   );
