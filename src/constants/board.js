f698350249bb7c66041ec20c9947a9a602af2fe2
haesa <xdhaesa@gmail.com>
2024-11-20 04:59:28 +0900

    #5 [REFACTOR] BOARD 상수의 키를 path로 변경

--- src/constants/board.js
+++ src/constants/board.js
@@ -1,8 +1,19 @@
+import { PATH } from './path.js';
+
 export const BOARD = Object.freeze({
-  all: {
+  [PATH.root]: {
     name: 'All Students',
+    title: 'Sookmyung Women’s Univ. All',
+    describe: '우리 학교에 재학 중인 모든 재학생 및 유학생을 만날 수 있습니다.',
   },
-  international: {
+  [PATH.allBoard]: {
+    name: 'All Students',
+    title: 'Sookmyung Women’s Univ. All',
+    describe: '우리 학교에 재학 중인 모든 재학생 및 유학생을 만날 수 있습니다.',
+  },
+  [PATH.internationalBoard]: {
     name: 'International Students',
+    title: 'Sookmyung Women’s Univ. International',
+    describe: '우리 학교에 재학 중인 유학생을 만날 수 있습니다.',
   },
 });
