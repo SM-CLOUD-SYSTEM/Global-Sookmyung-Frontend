e75ef06b9fed21096daf3420a78d53b39c387798
haesa <xdhaesa@gmail.com>
2024-11-20 19:35:08 +0900

    #7 [UI] BoardNavigationHeader 컴포넌트

--- src/constants/board.js
+++ src/constants/board.js
@@ -2,16 +2,19 @@ import { PATH } from './path.js';
 
 export const BOARD = Object.freeze({
   [PATH.root]: {
+    path: PATH.allBoard,
     name: 'All Students',
     title: 'Sookmyung Women’s Univ. All',
     describe: '우리 학교에 재학 중인 모든 재학생 및 유학생을 만날 수 있습니다.',
   },
   [PATH.allBoard]: {
+    path: PATH.allBoard,
     name: 'All Students',
     title: 'Sookmyung Women’s Univ. All',
     describe: '우리 학교에 재학 중인 모든 재학생 및 유학생을 만날 수 있습니다.',
   },
   [PATH.internationalBoard]: {
+    path: PATH.internationalBoard,
     name: 'International Students',
     title: 'Sookmyung Women’s Univ. International',
     describe: '우리 학교에 재학 중인 유학생을 만날 수 있습니다.',
--- src/pages/Post/index.jsx
+++ src/pages/Post/index.jsx
@@ -1,3 +1,15 @@
+import { Profile } from '@components';
+import BoardNavigationHeader from '@pages/Post/components/BoardNavigationHeader';
+
+import styles from './Post.module.css';
+
 export default function Post() {
-  return <section>Post</section>;
+  return (
+    <section className={styles.container}>
+      <Profile />
+      <article className={styles.post}>
+        <BoardNavigationHeader />
+      </article>
+    </section>
+  );
 }
