cf30948c859865d3d762cc59311b162284b3fea4
haesa <xdhaesa@gmail.com>
2024-11-20 22:25:29 +0900

    #7 [REFACTOR] post 더미 데이터를 dummy 폴더로 이동

--- src/pages/Post/index.jsx
+++ src/pages/Post/index.jsx
@@ -1,23 +1,9 @@
 import { Profile } from '@components';
 import BoardNavigationHeader from '@pages/Post/components/BoardNavigationHeader';
 import PostHeader from '@pages/Post/components/PostHeader';
+import { POST } from '@dummy';
 
 import styles from './Post.module.css';
-
-const title = '게시글제목';
-const profileImage = undefined;
-const nickname = '흑백요리사';
-const date = '2024.11.20';
-const likeCount = 100;
-const bookmarkCount = 100;
-const post = {
-  title,
-  profileImage,
-  nickname,
-  date,
-  likeCount,
-  bookmarkCount,
-};
 
 export default function Post() {
   return (
@@ -26,7 +12,7 @@ export default function Post() {
       <article className={styles.post}>
         <BoardNavigationHeader />
         <div className={styles.wrapper}>
-          <PostHeader post={post} />
+          <PostHeader post={POST} />
         </div>
       </article>
     </section>
