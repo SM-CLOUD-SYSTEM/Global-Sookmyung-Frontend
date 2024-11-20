d64e855c4c69385cbf97acd843517394d55739ac
haesa <xdhaesa@gmail.com>
2024-11-20 21:46:32 +0900

    #7 [REFACTOR] date 포맷 함수를 DateFormatter 클래스로 변환

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,7 +1,11 @@
-export const formatDateToMonthDay = (dateString) => {
-  const date = new Date(dateString);
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
+class DateFormatter {
+  static formatMonthDayKR(dateString) {
+    const date = new Date(dateString);
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
 
-  return `${month}월 ${day}일`;
-};
+    return `${month}월 ${day}일`;
+  }
+}
+
+export default DateFormatter;
