6e694346c941073070c1956c072869967dbcf5ee
haesa <xdhaesa@gmail.com>
2024-11-20 14:41:19 +0900

    #5 [STYLE] 루트 경로와 게시글 상세에서도 헤더의 메뉴가 활성화 되도록 수정

--- src/components/Header/BoardMenu.jsx
+++ src/components/Header/BoardMenu.jsx
@@ -1,13 +1,17 @@
 import { useLocation, Link } from 'react-router-dom';
 
 import styles from './BoardMenu.module.css';
+import { getDetailPathname } from '../../utils/path.js';
 
 export default function BoardMenu({ to, name }) {
   const { pathname } = useLocation();
-  const isSeleced = pathname === to ? styles.select : null;
+  const isSelected = () => {
+    const currentPath = getDetailPathname(pathname);
+    return currentPath.includes(to) ? styles.select : null;
+  };
 
   return (
-    <Link to={to} className={isSeleced}>
+    <Link to={to} className={isSelected()}>
       <li className={styles.menu}>
         <span className={styles.name}>{name}</span>
       </li>
