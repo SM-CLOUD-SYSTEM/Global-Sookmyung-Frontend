51b8ef7b3dcb33f9a6a3e5d036dc795748b7d8cc
haesa <xdhaesa@gmail.com>
2024-11-22 00:48:54 +0900

    #9 [MODIFY] 게시글 작성 페이지 라우트 변경

--- src/pages/Board/components/BoardHeader/index.jsx
+++ src/pages/Board/components/BoardHeader/index.jsx
@@ -1,17 +1,22 @@
-import { Link } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 
 import { Button } from '@components';
 
+import Path from '@utils/Path.js';
 import { BUTTON_SIZE, PATH } from '@constants';
 
 import styles from './BoardHeader.module.css';
 
 export default function BoardHeader() {
+  const { pathname } = useLocation();
+  const { path } = Path.getBoard(pathname);
+  const to = `${path}${PATH.postCreation}`;
+
   return (
     <div className={styles.boardHeader}>
       <h2 className={styles.boardTitle}>게시글</h2>
       <div className={styles.writeButton}>
-        <Link className={styles.link} to={PATH.postCreation}>
+        <Link className={styles.link} to={to}>
           <Button size={BUTTON_SIZE.small}>글쓰기</Button>
         </Link>
       </div>
