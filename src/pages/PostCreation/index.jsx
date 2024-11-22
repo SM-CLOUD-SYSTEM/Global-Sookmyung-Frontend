e196268e952721c9b0ced706901393a5d743819b
haesa <xdhaesa@gmail.com>
2024-11-22 17:13:18 +0900

    #9 [UI] 게시글 작성 페이지의 하단 버튼 스타일링

--- src/pages/PostCreation/index.jsx
+++ src/pages/PostCreation/index.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
-import { Profile } from '@components';
+import { Button, Profile, WhiteButton } from '@components';
 import { Dropdown } from '@pages/PostCreation/components';
 
 import Path from '@utils/Path.js';
@@ -17,42 +17,61 @@ export default function PostCreation() {
 
   const [boardId, setBoardId] = useState(board.id);
   const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
 
   const updateTitle = (event) => setTitle(event.target.value);
+  const updateContent = (event) => setContent(event.target.value);
+
+  const canCreate = boardId && title && content;
 
   return (
     <section className={styles.container}>
       <Profile />
       <article className={styles.article}>
-        <div className={styles.top}>
-          <div className={styles.category}>
-            <div className={styles.left}>
-              <span className={styles.label}>게시판</span>
+        <div className={styles.wrapper}>
+          <div className={styles.top}>
+            <div className={styles.category}>
+              <div className={styles.left}>
+                <span className={styles.label}>게시판</span>
+              </div>
+              <div className={styles.right}>
+                <Dropdown
+                  options={boards}
+                  setValue={setBoardId}
+                  initial={board}
+                />
+              </div>
             </div>
-            <div className={styles.right}>
-              <Dropdown
-                options={boards}
-                setValue={setBoardId}
-                initial={board}
-              />
+            <div className={styles.title}>
+              <div className={styles.left}>
+                <span className={styles.label}>제목</span>
+              </div>
+              <div className={styles.right}>
+                <input
+                  className={styles.input}
+                  type='text'
+                  value={title}
+                  onChange={updateTitle}
+                  placeholder='제목을 입력해주세요'
+                />
+              </div>
             </div>
           </div>
-          <div className={styles.title}>
-            <div className={styles.left}>
-              <span className={styles.label}>제목</span>
-            </div>
-            <div className={styles.right}>
-              <input
-                className={styles.input}
-                type='text'
-                value={title}
-                onChange={updateTitle}
-                placeholder='제목을 입력해주세요'
-              />
-            </div>
+          <div className={styles.bottom}>
+            <textarea
+              className={styles.content}
+              value={content}
+              onChange={updateContent}
+              placeholder='내용을 입력해주세요'
+            />
           </div>
         </div>
-        <div className={styles.bottom}></div>
+        <div className={styles.buttons}>
+          <div>
+            <WhiteButton>취소</WhiteButton>
+            <Button disabled={!canCreate}>등록</Button>
+          </div>
+        </div>
       </article>
     </section>
   );
