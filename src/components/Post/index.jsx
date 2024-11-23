b77762a8753c514edad40a2bd3154671c0bce7d9
haesa <xdhaesa@gmail.com>
2024-11-23 23:37:24 +0900

    #11 [REFACTOR] 라우트 이동 책임을 Post 컴포넌트로 이동

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,31 +1,35 @@
+import { Link } from 'react-router-dom';
+
 import DateFormatter from '@utils/Date.js';
 import { ReactComponent as LikeIcon } from '@assets/like.svg';
 
 import styles from './Post.module.css';
 
-export default function Post({ post }) {
+export default function Post({ post, to }) {
   const { postId, title, commentCount, date, writer, likeCount } = post;
 
   return (
-    <li className={styles.post}>
-      <div className={styles.left}>
-        <span className={styles.title}>{title}</span>
-        <span className={styles.commentCount}>({commentCount})</span>
-        <span className={styles.date}>
-          {DateFormatter.formatMonthDayKR(date)}
-        </span>
-      </div>
-      <div className={styles.right}>
-        <div>
-          <span className={styles.writer}>{writer}</span>
-        </div>
-        <div>
-          <span className={styles.likeCount}>
-            <LikeIcon />
-            {likeCount}
+    <Link className={styles.to} to={to}>
+      <li className={styles.post}>
+        <div className={styles.left}>
+          <span className={styles.title}>{title}</span>
+          <span className={styles.commentCount}>({commentCount})</span>
+          <span className={styles.date}>
+            {DateFormatter.formatMonthDayKR(date)}
           </span>
         </div>
-      </div>
-    </li>
+        <div className={styles.right}>
+          <div>
+            <span className={styles.writer}>{writer}</span>
+          </div>
+          <div>
+            <span className={styles.likeCount}>
+              <LikeIcon />
+              {likeCount}
+            </span>
+          </div>
+        </div>
+      </li>
+    </Link>
   );
 }
