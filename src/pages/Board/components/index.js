999a0e0764e9a997650b947a8a6de4c6820e7719
haesa <xdhaesa@gmail.com>
2024-11-23 23:28:45 +0900

    #11 [REFACTOR] Board 페이지에서 사용하던 Post를 공통 컴포넌트로 분리

--- src/pages/Board/components/index.js
+++ src/pages/Board/components/index.js
@@ -1,4 +1,3 @@
 export { default as BoardHeader } from '@pages/Board/components/BoardHeader';
 export { default as BoardIndex } from '@pages/Board/components/BoardIndex';
 export { default as Notice } from '@pages/Board/components/Notice';
-export { default as Post } from '@pages/Board/components/Post';
