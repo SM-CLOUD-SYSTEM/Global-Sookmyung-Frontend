8f7ed404476d599a381d10ac8ca83bf6c93ebcb9
haesa <xdhaesa@gmail.com>
2024-11-23 23:22:48 +0900

    #11 [UI] Info 컴포넌트

--- src/pages/MyPage/index.jsx
+++ src/pages/MyPage/index.jsx
@@ -1,6 +1,11 @@
-import { Group, Profile } from '@pages/MyPage/components';
+import { Group, Info, Profile } from '@pages/MyPage/components';
 
 import { PATH } from '@constants';
+import { ReactComponent as BookIcon } from '@assets/book.svg';
+import { ReactComponent as CallIcon } from '@assets/call.svg';
+import { ReactComponent as SchoolIcon } from '@assets/school.svg';
+
+import { INFO } from '@dummy/user.js';
 
 import styles from './MyPage.module.css';
 
@@ -10,7 +15,13 @@ export default function MyPage() {
       <Profile />
       <article className={styles.article}>
         <div className={styles.groups}>
-          <Group title='내 정보'></Group>
+          <Group title='내 정보'>
+            <div className={styles.myInfo}>
+              <Info Icon={SchoolIcon} label='학교' value={INFO.school} />
+              <Info Icon={BookIcon} label='학과' value={INFO.major} />
+              <Info Icon={CallIcon} label='번호' value={INFO.number} />
+            </div>
+          </Group>
           <Group
             title='내가 쓴 글'
             to={{
