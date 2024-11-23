9e353252c65f1445c5961d4c38d7105f34c71eb4
haesa <xdhaesa@gmail.com>
2024-11-23 23:22:48 +0900

    #11 [UI] Info 컴포넌트

--- src/dummy/user.js
+++ src/dummy/user.js
@@ -4,3 +4,9 @@ export const USER = Object.freeze({
   catchphrase: '메리크리',
   profileUrl: undefined,
 });
+
+export const INFO = Object.freeze({
+  school: '숙명여자대학교',
+  major: '컴퓨터과학전공',
+  number: '010-1234-5678',
+});
--- src/pages/MyPage/components/index.js
+++ src/pages/MyPage/components/index.js
@@ -1,2 +1,3 @@
 export { default as Group } from '@pages/MyPage/components/Group';
+export { default as Info } from '@pages/MyPage/components/Info';
 export { default as Profile } from '@pages/MyPage/components/Profile';
