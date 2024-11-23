0b8dac567cd41ae10e7462e46d95af865c88824b
haesa <xdhaesa@gmail.com>
2024-11-24 04:27:17 +0900

    #13 [UI] Category 컴포넌트

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,3 +1,48 @@
+import { useState } from 'react';
+
+import { useSearch } from '@hooks';
+import { BoardIndex, Post, Profile } from '@components';
+import { Category, SearchInput } from '@pages/Search/components';
+
+import { ReactComponent as ManageSearch } from '@assets/manageSearch.svg';
+import { SEARCH_POSTS } from '@dummy';
+
+import styles from './Search.module.css';
+
 export default function Search() {
-  return <section>검색 페이지</section>;
+  const { keyword, category, updateCategory, search } = useSearch();
+
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchKeyword, setSearchKeyword] = useState('고구마');
+
+  return (
+    <section className={styles.container}>
+      <Profile />
+      <article className={styles.article}>
+        <div className={styles.search}>
+          <div className={styles.title}>
+            <ManageSearch />
+            <h2>
+              <span className={styles.keyword}>
+                "<span className={styles.underline}>{keyword}</span>"
+              </span>
+              검색 결과입니다.
+            </h2>
+          </div>
+          <SearchInput keyword={keyword} search={search} />
+        </div>
+        <div className={styles.result}>
+          <Category category={category} updateCategory={updateCategory} />
+          <div className={styles.board}>
+            <BoardIndex />
+            <ul>
+              {searchResults.map((post) => (
+                <Post post={post} />
+              ))}
+            </ul>
+          </div>
+        </div>
+      </article>
+    </section>
+  );
 }
