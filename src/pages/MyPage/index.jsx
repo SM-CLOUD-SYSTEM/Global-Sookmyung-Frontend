9115af170d06c4c3de36bb6f6acca6c9a95f05fc
haesa <xdhaesa@gmail.com>
2024-11-23 20:59:20 +0900

    #11 [UI] Group 컴포넌트

--- src/pages/MyPage/index.jsx
+++ src/pages/MyPage/index.jsx
@@ -1,4 +1,6 @@
-import { Profile } from '@pages/MyPage/components';
+import { Group, Profile } from '@pages/MyPage/components';
+
+import { PATH } from '@constants';
 
 import styles from './MyPage.module.css';
 
@@ -6,7 +8,25 @@ export default function MyPage() {
   return (
     <section className={styles.container}>
       <Profile />
-      <article className={styles.article}></article>
+      <article className={styles.article}>
+        <div className={styles.groups}>
+          <Group title='내 정보'></Group>
+          <Group
+            title='내가 쓴 글'
+            to={{
+              path: PATH.myPost,
+              label: '전체보기',
+            }}
+          ></Group>
+          <Group
+            title='즐겨찾기한 글'
+            to={{
+              path: PATH.bookmark,
+              label: '전체보기',
+            }}
+          ></Group>
+        </div>
+      </article>
     </section>
   );
 }
