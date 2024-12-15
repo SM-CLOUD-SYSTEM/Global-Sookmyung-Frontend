2f7a4459fcdcea2af4fc14cbcfa5200e2b06d098
haesa <xdhaesa@gmail.com>
2024-12-16 01:34:45 +0900

    #28 [FEAT] 댓글 생성 API 연결

--- src/apis/comment.js
+++ src/apis/comment.js
@@ -4,3 +4,10 @@ export const getComments = async ({ postId }) => {
   const response = await authAxios.get(`/comments/posts/${postId}`);
   return response?.result.data;
 };
+
+export const createComment = async ({ postId, content }) => {
+  const response = await authAxios.post(`/comments/posts/${postId}`, {
+    content,
+  });
+  return response;
+};
--- src/pages/Post/components/CommentInput/index.jsx
+++ src/pages/Post/components/CommentInput/index.jsx
@@ -1,24 +1,44 @@
 import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useQueryClient, useMutation } from '@tanstack/react-query';
+
+import { createComment } from '@apis';
 
 import { Button } from '@components';
 
 import { BUTTON_SIZE } from '@constants';
 
 import styles from './CommentInput.module.css';
+import { MUTATION_KEY, QUERY_KEY } from '../../../../constants/query.js';
 
 const MAX_LENGTH = 1000;
 
 export default function CommentInput() {
-  const [text, setText] = useState('');
-  const onChange = (event) => setText(event.target.value);
+  const [content, setContent] = useState('');
+  const onChange = (event) => setContent(event.target.value);
+
+  const { postId } = useParams();
+  const queryClient = useQueryClient();
+  const create = useMutation({
+    mutationKey: MUTATION_KEY.createComment,
+    mutationFn: async () => await createComment({ postId, content }),
+    onSuccess: () => {
+      queryClient.invalidateQueries([QUERY_KEY.comments, postId]);
+    },
+    onError: (error) => {
+      alert(error.response.data.message);
+    },
+  });
+
   const onSubmit = () => {
-    if (!text) {
+    if (!content) {
       return;
     }
 
-    console.log(text);
-    setText('');
+    create.mutate();
+    setContent('');
   };
+
   const onKeyDown = (event) => {
     const isEnter = event.key === 'Enter';
     const isMac = navigator.platform.includes('Mac');
@@ -35,14 +55,14 @@ export default function CommentInput() {
       <div className={styles.left}>
         <textarea
           className={styles.textarea}
-          value={text}
+          value={content}
           onChange={onChange}
           onKeyDown={onKeyDown}
           maxLength={MAX_LENGTH}
           placeholder='댓글을 입력해주세요'
         />
         <span className={styles.maxLength}>
-          {text.length}/{MAX_LENGTH}
+          {content.length}/{MAX_LENGTH}
         </span>
       </div>
       <div className={styles.right}>
