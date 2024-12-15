97718fff25abf9e6a4521500e3efc84664002837
haesa <xdhaesa@gmail.com>
2024-12-16 05:02:43 +0900

    #35 [FEAT] 검색 API 연결

--- src/pages/Search/components/index.js
+++ src/pages/Search/components/index.js
@@ -1,2 +1,4 @@
 export { default as Category } from '@pages/Search/components/Category';
+export { default as Posts } from '@pages/Search/components/Posts';
+export { default as PostsErrorFallback } from '@pages/Search/components/Posts/PostsErrorFallback';
 export { default as SearchInput } from '@pages/Search/components/SearchInput';
