66931a971d633db923d99fb61a884acb0cf335b6
haesa <xdhaesa@gmail.com>
2024-12-15 19:53:30 +0900

    #23 [FEAT] 북마크 API 연결

--- src/apis/bookmark.js
+++ src/apis/bookmark.js
@@ -1,1 +1,11 @@
 import { authAxios } from '@/axios';
+
+export const bookmark = async ({ postId }) => {
+  const response = await authAxios.post(`/bookmarks/posts/${postId}`);
+  return response;
+};
+
+export const unbookmark = async ({ postId }) => {
+  const response = await authAxios.delete(`/bookmarks/posts/${postId}`);
+  return response;
+};
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,3 +1,4 @@
+export { default as useBookmark } from '@hooks/useBookmark.jsx';
 export { default as useFunnel } from '@hooks/useFunnel.jsx';
 export { default as useLike } from '@hooks/useLike.jsx';
 export { default as useSearch } from '@hooks/useSearch.jsx';
