7e2d54319e5a8af2591e707cbe077f16e94849a5
haesa <xdhaesa@gmail.com>
2024-12-15 19:53:30 +0900

    #23 [FEAT] 북마크 API 연결

--- src/pages/Post/components/BookmarkButton/index.jsx
+++ src/pages/Post/components/BookmarkButton/index.jsx
@@ -1,31 +1,35 @@
 import { useState } from 'react';
 
+import { useBookmark } from '@hooks';
 import ActionButton from '@pages/Post/components/ActionButton';
 
 import { ReactComponent as BookmarkActiveIcon } from '@assets/bookmarkActive.svg';
 import { ReactComponent as BookmarkInactiveIcon } from '@assets/bookmark.svg';
 
 export default function BookmarkButton({ bookmarked, count }) {
+  const { bookmark, unbookmark } = useBookmark();
   const [isBookmark, setIsBookmark] = useState(bookmarked);
   const [bookmarkCount, setBookmarkCount] = useState(count);
 
-  const bookmark = () => {
+  const onBookmark = async () => {
+    await bookmark.mutate();
     setIsBookmark(true);
     setBookmarkCount((prev) => prev + 1);
   };
 
-  const unbookmark = () => {
+  const onUnbookmark = async () => {
+    await unbookmark.mutate();
     setIsBookmark(false);
     setBookmarkCount((prev) => prev - 1);
   };
 
   const onClick = () => {
     if (isBookmark) {
-      unbookmark();
+      onUnbookmark();
       return;
     }
 
-    bookmark();
+    onBookmark();
   };
 
   return (
