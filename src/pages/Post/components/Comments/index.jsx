d1099a4686d910dba6611fa60eb07ca4b27223c1
haesa <xdhaesa@gmail.com>
2024-12-16 01:17:17 +0900

    #28 [FEAT] 댓글 조회 API 연결

--- src/pages/Post/components/Comments/index.jsx
+++ src/pages/Post/components/Comments/index.jsx
@@ -1,28 +1,30 @@
 import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useSuspenseQuery } from '@tanstack/react-query';
+
+import { getComments } from '@apis';
 
 import { Paginator } from '@components';
 import { Comment } from '@pages/Post/components';
 
-import { COMMENTS } from '@dummy';
-
-import styles from './Comments.module.css';
+import { QUERY_KEY } from '../../../../constants/query.js';
 
 const START_PAGE = 1;
 const pageCount = 117;
 
 export default function Comments() {
+  const { postId } = useParams();
+  const { data: comments } = useSuspenseQuery({
+    queryKey: [QUERY_KEY.comments, postId],
+    queryFn: async () => await getComments({ postId }),
+  });
   const [page, setPage] = useState(START_PAGE);
 
   return (
-    <div>
-      <ul>
-        {COMMENTS.map((comment) => (
-          <Comment key={comment.commentId} comment={comment} />
-        ))}
-      </ul>
-      <div className={styles.paginator}>
-        <Paginator page={page} setPage={setPage} pageCount={pageCount} />
-      </div>
-    </div>
+    <ul>
+      {comments.map((comment) => (
+        <Comment key={comment.commentId} comment={comment} />
+      ))}
+    </ul>
   );
 }
--- src/pages/Post/components/index.js
+++ src/pages/Post/components/index.js
@@ -3,6 +3,7 @@ export { default as BoardNavigationHeader } from '@pages/Post/components/BoardNa
 export { default as BookmarkButton } from '@pages/Post/components/BookmarkButton';
 export { default as Comment } from '@pages/Post/components/Comment';
 export { default as Comments } from '@pages/Post/components/Comments';
+export { default as CommentsErrorFallback } from '@pages/Post/components/Comments/CommentsErrorFallback';
 export { default as CommentInput } from '@pages/Post/components/CommentInput';
 export { default as LikeButton } from '@pages/Post/components/LikeButton';
 export { default as PostHeader } from '@pages/Post/components/PostHeader';
