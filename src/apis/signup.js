b498149c09c02e457f62a845977088c6e4e723df
haesa <xdhaesa@gmail.com>
2024-12-17 04:30:38 +0900

    #43 [FEAT] 이메일 인증코드 검증 API 연결

--- src/apis/signup.js
+++ src/apis/signup.js
@@ -15,3 +15,27 @@ export const sendEmailCode = async ({ email }) => {
     return { message: '잠시 후 다시 시도해주세요', isSuccess: false };
   }
 };
+
+export const checkEmailCode = async ({ email, code }) => {
+  try {
+    const response = await defaultAxios.post('/auth/verify/email', {
+      email,
+      code,
+    });
+    const { accessToken: guestToken } = response?.result.data;
+
+    return {
+      message: '인증이 완료되었습니다',
+      isSuccess: true,
+      guestToken,
+    };
+  } catch (error) {
+    const { status } = error?.response ?? {};
+
+    if (status === 400) {
+      return { message: '인증번호가 유효하지 않습니다', isSuccess: false };
+    }
+
+    return { message: '잠시 후 다시 시도해주세요', isSuccess: false };
+  }
+};
