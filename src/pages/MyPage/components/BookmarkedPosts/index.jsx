17e5c46ce09bc0d8bb4bfee85601c9d2d8c35fd3
haesa <xdhaesa@gmail.com>
2024-12-17 08:14:30 +0900

    #74 [FEAT] 즐겨찾기한 글 조회 API 연결

---
+++ src/pages/MyPage/components/BookmarkedPosts/index.jsx
@@ -0,0 +1,37 @@
+import { useSuspenseQuery } from '@tanstack/react-query';
+
+import { getBookmarkedPosts } from '@apis';
+
+import { Post } from '@components';
+import { Group } from '@pages/MyPage/components';
+import Path from '@utils/Path';
+import { PATH, QUERY_KEY } from '@constants';
+
+export default function BookmarkedPosts() {
+  const { data } = useSuspenseQuery({
+    queryKey: [QUERY_KEY.bookmarkedPosts],
+    queryFn: async () => await getBookmarkedPosts(),
+  });
+
+  const posts = data.slice(0, 5);
+
+  return (
+    <Group
+      title='즐겨찾기한 글'
+      to={{
+        path: PATH.bookmark,
+        label: '전체보기',
+      }}
+    >
+      {posts.map((post) => (
+        <Post
+          post={post}
+          to={Path.getPostPath({
+            postId: post.postId,
+            boardId: post.boardId,
+          })}
+        />
+      ))}
+    </Group>
+  );
+}
