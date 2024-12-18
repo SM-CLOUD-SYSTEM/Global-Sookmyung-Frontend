cc8c280acd23f4d8bbb5a3624fbde8565041e03c
haesa <xdhaesa@gmail.com>
2024-12-19 00:36:18 +0900

    [FIX] 로그인, 좋아요, 북마크 에러 처리 로직 수정

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,3 +1,1 @@
-export { default as useBookmark } from '@hooks/useBookmark.jsx';
 export { default as useFunnel } from '@hooks/useFunnel.jsx';
-export { default as useLike } from '@hooks/useLike.jsx';
--- src/pages/Post/components/BookmarkButton/index.jsx
+++ src/pages/Post/components/BookmarkButton/index.jsx
@@ -1,35 +1,65 @@
 import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useMutation } from '@tanstack/react-query';
 
-import { useBookmark } from '@hooks';
+import { bookmark as bookmarkApi, unbookmark as unbookmarkApi } from '@/apis';
+
 import ActionButton from '@pages/Post/components/ActionButton';
 
+import { MUTATION_KEY } from '@/constants';
 import { ReactComponent as BookmarkActiveIcon } from '@assets/bookmarkActive.svg';
 import { ReactComponent as BookmarkInactiveIcon } from '@assets/bookmark.svg';
 
 export default function BookmarkButton({ bookmarked, count }) {
-  const { bookmark, unbookmark } = useBookmark();
+  const { postId } = useParams();
   const [isBookmark, setIsBookmark] = useState(bookmarked);
   const [bookmarkCount, setBookmarkCount] = useState(count);
 
-  const onBookmark = async () => {
-    await bookmark.mutate();
-    setIsBookmark(true);
-    setBookmarkCount((prev) => prev + 1);
-  };
+  const bookmark = useMutation({
+    mutationKey: [MUTATION_KEY.scrap],
+    mutationFn: async () => await bookmarkApi({ postId }),
+    onSuccess: () => {
+      setIsBookmark(true);
+      setBookmarkCount((prev) => prev + 1);
+    },
+    onError: (error) => {
+      const { status } = error?.response ?? {};
 
-  const onUnbookmark = async () => {
-    await unbookmark.mutate();
-    setIsBookmark(false);
-    setBookmarkCount((prev) => prev - 1);
-  };
+      if (status === 401) {
+        alert('로그인 후 이용해주세요');
+        return;
+      }
 
-  const onClick = () => {
+      alert('잠시 후 다시 시도해주세요');
+    },
+  });
+
+  const unbookmark = useMutation({
+    mutationKey: [MUTATION_KEY.bookmark],
+    mutationFn: async () => await unbookmarkApi({ postId }),
+    onSuccess: () => {
+      setIsBookmark(false);
+      setBookmarkCount((prev) => prev - 1);
+    },
+    onError: (error) => {
+      const { status } = error?.response ?? {};
+
+      if (status === 401) {
+        alert('로그인 후 이용해주세요');
+        return;
+      }
+
+      alert('잠시 후 다시 시도해주세요');
+    },
+  });
+
+  const onClick = async () => {
     if (isBookmark) {
-      onUnbookmark();
+      await unbookmark.mutate();
       return;
     }
 
-    onBookmark();
+    await bookmark.mutate();
   };
 
   return (
