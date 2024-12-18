2b1408d5d5f977ad6fc3b30e975f8e8194649d01
haesa <xdhaesa@gmail.com>
2024-12-18 18:30:35 +0900

    #124 [FIX] 댓글 작성에 누락된 에러 처리 로직 추가

--- src/pages/Post/components/CommentInput/index.jsx
+++ src/pages/Post/components/CommentInput/index.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { useQueryClient, useMutation } from '@tanstack/react-query';
 
 import { createComment } from '@apis';
@@ -14,11 +14,14 @@ import { MUTATION_KEY, QUERY_KEY } from '../../../../constants/query.js';
 const MAX_LENGTH = 1000;
 
 export default function CommentInput() {
+  const navigate = useNavigate();
+
   const [content, setContent] = useState('');
   const onChange = (event) => setContent(event.target.value);
 
   const { postId } = useParams();
   const queryClient = useQueryClient();
+
   const create = useMutation({
     mutationKey: MUTATION_KEY.createComment,
     mutationFn: async ({ content }) => await createComment({ postId, content }),
@@ -26,7 +29,16 @@ export default function CommentInput() {
       queryClient.invalidateQueries([QUERY_KEY.comments, postId]);
     },
     onError: (error) => {
-      alert(error.response.data.message);
+      const { status } = error?.response;
+
+      if (status === 401) {
+        alert('로그인 후 이용가능합니다');
+        navigate(-1);
+        return;
+      }
+
+      alert('잠시 후 다시 시도해주세요');
+      return;
     },
   });
 
