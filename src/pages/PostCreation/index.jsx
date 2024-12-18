c5a81afcbf2bc6dadffa10feeed6d0a202d9fba8
haesa <xdhaesa@gmail.com>
2024-12-18 21:01:46 +0900

    [FIX] 게시글 작성 onError에 누락된 error 객체 전달

--- src/pages/PostCreation/index.jsx
+++ src/pages/PostCreation/index.jsx
@@ -41,7 +41,7 @@ export default function PostCreation() {
 
       navigate(boardPath);
     },
-    onError: () => {
+    onError: (error) => {
       const { status } = error?.response ?? {};
 
       if (status === 401) {
